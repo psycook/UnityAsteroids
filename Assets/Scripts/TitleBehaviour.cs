bb5bdd1c108142fe3a638860a90773eb78c90392
Simon Cook <simoncook@Simons-MacBook-Pro.local>
2024-07-20 09:33:35 +0100

    Title animiation

---
+++ Assets/Scripts/TitleBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleBehaviour : MonoBehaviour
+{
+    private Transform _transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // add a floating effect to the title change position and rotation slightly
+        _transform.position = new Vector3(_transform.position.x, _transform.position.y + Mathf.Sin(Time.time) * 0.005f, _transform.position.z);
+        _transform.rotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time) * 2.0f);
+    }
+}
