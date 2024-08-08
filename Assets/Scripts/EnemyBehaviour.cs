18e42c461a1992103d41d036bb54eecdc50ac9e9
Simon Cook <simoncook@microsoft.com>
2024-08-08 17:38:20 +0100

    updated enemy shooting rate

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -6,7 +6,8 @@ public class EnemyBehaviour : MonoBehaviour
     [SerializeField] private float MaxSpeed = 150.0f;
     [SerializeField] public int Points = 100;
     [SerializeField] private ParticleSystem explosionParticles;
-    [SerializeField] private int ShootFrequency = 200;
+    [SerializeField] private float ShootMinRate = 1.0f;
+    [SerializeField] private float ShootMaxRate = 5.0f;
     [SerializeField] private GameObject MissilePrefab;
     [SerializeField] private float MissleSpeed = 100.0f;
     [SerializeField] private AudioClip SmallEnemyAudioClip;
@@ -18,7 +19,9 @@ public class EnemyBehaviour : MonoBehaviour
     private EnemySize[] PossibleSizesArray = new EnemySize[] { EnemySize.Small, EnemySize.Large, EnemySize.Large };
     private EnemySize Size;
     private EnemyMissilePool MissilePool;
-
+    private float currentShootRate = 5.0f;
+    private float shootTimer = 0.0f;
+    private EnemySize currentSize;
 
     void Start()
     {
@@ -33,6 +36,7 @@ public class EnemyBehaviour : MonoBehaviour
                 }
                 transform.localScale = new Vector3(0.3f, 0.22f, 0.5f);
                 Points = 200;
+                currentSize = EnemySize.Small;
                 break;
             case EnemySize.Large:
                 if (LargeEnemyAudioClip)
@@ -41,9 +45,9 @@ public class EnemyBehaviour : MonoBehaviour
                 }
                 transform.localScale = new Vector3(0.4f, 0.33f, 1.0f);
                 Points = 100;
+                currentSize = EnemySize.Large;
                 break;
         }
-
         ScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         Width = transform.GetComponent<LineRenderer>().bounds.extents.x;
         Height = transform.GetComponent<LineRenderer>().bounds.extents.y;
@@ -51,6 +55,7 @@ public class EnemyBehaviour : MonoBehaviour
         GetComponent<Rigidbody2D>().AddForce(transform.right * Random.Range(MinSpeed, MaxSpeed));
         ParentScript = GetComponentInParent<WaveBehaviour>();
         MissilePool = GameObject.Find("EnemyMissilePool").GetComponent<EnemyMissilePool>();
+        SetCurrentShootRate();
     }
 
     void Update()
@@ -74,25 +79,13 @@ public class EnemyBehaviour : MonoBehaviour
         }
         transform.position = viewPos;
 
-        if (Random.Range(0, ShootFrequency) == 0)
+        shootTimer += Time.deltaTime;
+
+        if(shootTimer >= currentShootRate)
         {
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
-            Vector3 playerPosition = new Vector3(0, 0, 0);
-            if (player)
-            {
-                playerPosition = player.transform.position;
-            }
-            float angle = Random.Range(0, 360);
-            float distance = Random.Range(1.0f, (Size == EnemySize.Small) ? 2.0f : 4.0f);
-            Vector3 targetPosition = new Vector3(playerPosition.x + Mathf.Cos(angle) * distance, playerPosition.y + Mathf.Sin(angle) * distance, 0);
-            Vector3 direction = (targetPosition - transform.position).normalized;
-            GameObject missile = MissilePool.GetMissile();
-            if (missile != null)
-            {
-                missile.transform.position = transform.position;
-                missile.transform.rotation = Quaternion.identity;
-                missile.GetComponent<Rigidbody2D>().AddForce(direction * MissleSpeed);
-            }
+            Shoot();
+            shootTimer = 0.0f;
+            SetCurrentShootRate();
         }
     }
 
@@ -113,4 +106,39 @@ public class EnemyBehaviour : MonoBehaviour
             Debug.Log("ParentScript is null");
         }
     }
+
+    private void SetCurrentShootRate()
+    {
+        switch (currentSize)
+        {
+            case EnemySize.Small:
+                currentShootRate = Random.Range(ShootMinRate/2, ShootMaxRate/2);
+                break;
+            case EnemySize.Large:
+                currentShootRate = Random.Range(ShootMinRate, ShootMaxRate);
+                break;
+        }
+    }
+
+    private void Shoot() 
+    {
+           GameObject player = GameObject.FindGameObjectWithTag("Player");
+            Vector3 playerPosition = new Vector3(0, 0, 0);
+            if (player)
+            {
+                playerPosition = player.transform.position;
+            }
+            float angle = Random.Range(0, 360);
+            float distance = Random.Range(1.0f, (Size == EnemySize.Small) ? 2.0f : 4.0f);
+            Vector3 targetPosition = new Vector3(playerPosition.x + Mathf.Cos(angle) * distance, playerPosition.y + Mathf.Sin(angle) * distance, 0);
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            GameObject missile = MissilePool.GetMissile();
+            if (missile != null)
+            {
+                missile.transform.position = transform.position;
+                missile.transform.rotation = Quaternion.identity;
+                missile.GetComponent<Rigidbody2D>().AddForce(direction * MissleSpeed);
+            }
+    }
+
 }
